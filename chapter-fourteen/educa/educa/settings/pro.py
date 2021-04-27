28bb6a0379e6fb12e74851126388f543d8eb4871
BikoPython <stevennyandoya@gmail.com>
2021-04-27 08:44:11 +0300

    additions: database-setup

--- chapter-fourteen/educa/educa/settings/pro.py
+++ chapter-fourteen/educa/educa/settings/pro.py
@@ -10,6 +10,9 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'educa',
+        'PASSWORD': 'educa',
     }
 }
