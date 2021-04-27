2cfd161b08396150d1f2044caa0656df9771890e
BikoPython <stevennyandoya@gmail.com>
2021-04-27 12:44:53 +0300

    additions:serving-static-media-assets

--- chapter-fourteen/educa/educa/settings/base.py
+++ chapter-fourteen/educa/educa/settings/base.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
